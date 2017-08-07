27e80723cbdfc5f2553199496793f324dff1e33e
Suman Debnath <suman.magic97@gmail.com>
2017-08-07 11:31:05 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/demoDemo', function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
   res.send("demoDemo");
 });
 
